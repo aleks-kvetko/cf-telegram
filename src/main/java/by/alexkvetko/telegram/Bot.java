cf66f49fc4f5d33fd56927a05da84afbbc38802e
Alex Kvetko <alex.kvetko@helmes.ee>
2018-04-12 18:20:28 +0300

    Echo service

--- src/main/java/by/alexkvetko/telegram/Bot.java
+++ src/main/java/by/alexkvetko/telegram/Bot.java
@@ -14,19 +14,12 @@ public class Bot extends TelegramLongPollingBot {
   private String token;
 
   public void onUpdateReceived(Update update) {
-
-    // We check if the update has a message and the message has text
     if (update.hasMessage() && update.getMessage().hasText()) {
-      // Set variables
-      String message_text = update.getMessage().getText();
-      long chat_id = update.getMessage().getChatId();
-      String messageToSend = new StringBuffer(message_text).reverse().toString();
-
-      SendMessage message = new SendMessage() // Create a message object object
-          .setChatId(chat_id)
-          .setText(messageToSend);
+      SendMessage message = new SendMessage()
+          .setChatId(update.getMessage().getChatId())
+          .setText(update.getMessage().getText());
       try {
-        execute(message); // Sending our message object to user
+        execute(message);
       } catch (TelegramApiException e) {
         e.printStackTrace();
       }
